3eabc139b1a165b43a9f76412e9a5c84a0e1ce16
jeanfrancoisgratton <jean-francois@famillegratton.net>
2024-04-05 10:53:50 -0400

    Build script has been rewritten to add the branch name to binary

--- src/build.sh
+++ src/build.sh
@@ -1,9 +1,22 @@
 #!/usr/bin/env sh
 
+GOROOT=/opt/go
+OUTPUT=/opt/bin
 
+# Get git branch's name, replace / with _
+BRANCH=`git rev-parse --abbrev-ref HEAD`
+BRANCH=$(echo "$BRANCH" | tr '/' '_')
 
-if [ "$#" -gt 0 ]; then
-    BINARY=portainerImporter
+BINARY=portainerImporter
+
+if [ "$BRANCH" = "master" ] || [ "$BRANCH" = "main" ] || [ "$BRANCH" = "develop" ]; then
+    FULLNAME="$BINARY"
+else
+    FULLNAME="$BINARY-$BRANCH"
 fi
 
-go build -o /opt/bin/${BINARY} .
+if [ "$#" -gt 0 ]; then
+    OUTPUT=$1
+fi
+
+go build -o $OUTPUT/$FULLNAME .
